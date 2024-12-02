a33326e0022b042b18faa532566c4172ccdfe49d
Anders Tomasson <117904283+AtAce22@users.noreply.github.com>
2024-12-02 13:13:17 +0100

    Added loop to passenger, fixed seats issue

--- passenger.py
+++ passenger.py
@@ -3,15 +3,16 @@ import grpc
 import coster_pb2_grpc
 import coster_pb2
 
-passenger_id = "passenger_1"
-server_address = "192.168.1.73:54321" # change 192.168.1.73 to dedicated server
-wagon_address = "localhost:54322"
+server_address = "localhost:54321"
+wagon_address = "192.168.1.73:54322"
 
 class Client:
     def __init__(self, server_address):
         self.server_address = server_address
         self.channel = grpc.aio.insecure_channel(server_address)
         self.stub = coster_pb2_grpc.RollerCoasterStub(self.channel)
+        self.seats_per_wagon = 4
+        self.passenger_count = 0
 
     async def subscribe(self, passenger_id):
         try:
@@ -19,7 +20,7 @@ class Client:
             if response.value:
                 print(f"{passenger_id} subscribed successfully.")
             else:
-                print(f"{passenger_id} Failed to subscribe.")
+                print(f"{passenger_id} failed to subscribe.")
         except Exception as e:
             print(f"{passenger_id} Error subscribing, error type: {e}")
 
@@ -29,7 +30,7 @@ class Client:
             if response.value:
                 print(f"{passenger_id} unsubscribed successfully.")
             else:
-                print(f"{passenger_id} Failed to unsubscribe.")
+                print(f"{passenger_id} failed to unsubscribe.")
         except Exception as e:
             print(f"{passenger_id} Error unsubscribing, error type: {e}")
 
@@ -39,35 +40,55 @@ class Client:
                 wagon_stub = coster_pb2_grpc.WagonStub(channel)
                 response = await wagon_stub.Board(coster_pb2.Boarding(topic="boarding", _id=passenger_id))
                 if response.value:
-                    print(f"{passenger_id} Boarded wagon successfully.")
+                    self.passenger_count += 1
+                    print(f"{passenger_id} boarded wagon successfully.")
+                    if self.passenger_count == self.seats_per_wagon:
+                        await self.i_am_departing(wagon_address, "wagon_1") 
+                        self.passenger_count = 0
                 else:
-                    print(f"{passenger_id} Failed to board.")
+                    print(f"{passenger_id} failed to board.")
         except Exception as e:
             print(f"{passenger_id} Error boarding, error type: {e}")
+
+    async def i_am_departing(self, wagon_address, wagon_id):
+        try:
+            async with grpc.aio.insecure_channel(wagon_address) as channel:
+                wagon_stub = coster_pb2_grpc.WagonStub(channel)
+                response = await wagon_stub.Depart(coster_pb2.Departure(topic="departure", _id=wagon_id))
+                if response.value:
+                    print(f"Wagon {wagon_id} departed successfully.")
+                else:
+                    print(f"Wagon {wagon_id} failed to depart.")
+        except Exception as e:
+            print(f"Wagon {wagon_id} Error departing, error type: {e}")
 
     async def i_am_disembarking(self, passenger_id):
         try:
             response = await self.stub.Disembark(coster_pb2.Disembark_(topic="disembarking", _id=passenger_id))
             if response.value:
-                print(f"{passenger_id} Disembarked successfully.")
+                print(f"{passenger_id} disembarked successfully.")
             else:
-                print(f"{passenger_id} Failed to disembark.")
+                print(f"{passenger_id} failed to disembark.")
         except Exception as e:
             print(f"{passenger_id} Error disembarking, error type: {e}")
 
 async def main():
-    #server = grpc.aio.server()
-    #coster_pb2_grpc.add_ReceiverServicer_to_server(Client(), server)
-
     passenger_client = Client(server_address)
+    
+    while True:
+        for num in range(1, 6):
+            passenger_id = f"passenger_{num}"
+            await passenger_client.subscribe(passenger_id)
+            await asyncio.sleep(1)
+            await passenger_client.i_am_boarding(wagon_address, passenger_id)
+            await asyncio.sleep(1)
 
-    #testing the implementation 
-    await passenger_client.subscribe(passenger_id)
-    await asyncio.sleep(2)
-    await passenger_client.i_am_boarding(wagon_address, passenger_id)
-    await asyncio.sleep(5)
-    await passenger_client.i_am_disembarking(passenger_id)
-    await passenger_client.unsubscribe(passenger_id)
+        for num in range(1, 6):
+            passenger_id = f"passenger_{num}"
+            await passenger_client.i_am_disembarking(passenger_id)
+            await asyncio.sleep(1)
+            await passenger_client.unsubscribe(passenger_id)
+            await asyncio.sleep(1)
 
 if __name__ == "__main__":
     asyncio.run(main())
